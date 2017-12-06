d98bd71c1392db9683b7af807b77f749bdd84cf6
Patrick Edelen <patrickedelen@gmail.com>
2017-12-05 21:38:53 -0600

    basic graphql server

--- graphql/gql/app.js
+++ graphql/gql/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import schema from './graphql/schema';
+import schema from './src/schema';
 
 const app = express();
 const dev = process.env.NODE_ENV === 'development';
